7e150067921b88cc8c32f3616b90b89b5f2c68dc
Hoshiyar Jyani <hoshiyarjyani@gmail.com>
2022-08-08 03:43:36 +0530

    Print 1 to 100 counting using (While Loop)

---
+++ whileloopstructure.java
@@ -0,0 +1,10 @@
+public class whileloopstructure {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
