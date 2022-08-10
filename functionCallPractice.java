23465695f3b4487f5628ee463a1a9a50af573a70
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-10 18:52:23 +0530

    What is function How to use/ How to call for print name

---
+++ functionCallPractice.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class functionCallPractice {
+
+    public static void printMyName(String name) {
+        System.out.println(name);
+          return;
+    }
+    
+        public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);  
+}
+}
