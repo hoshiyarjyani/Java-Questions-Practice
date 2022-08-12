c0f358e4ce24a8f6636288133f5c934e9820943d
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-13 00:05:50 +0530

    sum of n odd no  from  funcation from  scanner

---
+++ oddnosumFunction.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//Write a function to print the sum of all odd numbers from 1 to n.
+
+
+
+public class oddnosumFunction {
+    
+public static int sumofOddno(int n){
+        System.out.print(n*n);
+    return n;
+    
+    
+}
+
+public static void main(String args[]) {
+    Scanner sc= new Scanner(System.in);
+    int n = sc.nextInt();
+    sumofOddno(n);
+}
+
+}
