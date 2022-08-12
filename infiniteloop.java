95af6db44821f3d73c19da17c940721d5d9154fc
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-13 00:56:09 +0530

    Write an infinite loop using do while condition.

---
+++ infiniteloop.java
@@ -0,0 +1,11 @@
+//Write an infinite loop using do while condition.
+public class infiniteloop {
+    public static void main(String args[]) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i > 0);
+
+    }
+}
