308ab23344a2e7be1b15206dcfba0f94148acb14
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-14 04:13:25 +0530

    set char and Get char Notes

---
+++ setcharandgetCharinjava.java
@@ -0,0 +1,17 @@
+Notes- set char and get char
+
+
+// Get A Character from Index
+
+// StringBuilder sb = new StringBuilder("Tony");
+//      //Set Char
+//      System.out.println(sb.charAt(0));
+ 
+
+// Set a Character at Index
+
+// StringBuilder sb = new StringBuilder("Tony");
+//      //Get Char
+//      sb.setCharAt(0, 'P');
+//      System.out.println(sb);
+ 
