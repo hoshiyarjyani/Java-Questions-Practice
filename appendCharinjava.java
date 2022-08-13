609835c917fe77197ba918665a64f189ad319787
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-14 04:07:26 +0530

    Append in string in java

---
+++ appendCharinjava.java
@@ -0,0 +1,10 @@
+//Append a char 
+//Append means to add something at the end.
+ 
+public class appendCharinjava {
+   public static void main(String args[]) {
+     StringBuilder sb = new StringBuilder("Tony");
+     sb.append(" Stark");
+     System.out.println(sb); 
+   }
+}
