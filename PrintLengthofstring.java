80637919ccd60928a89cf6a7d8b1a33e415a5e40
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-14 04:02:31 +0530

    Print Length of String

---
+++ PrintLengthofstring.java
@@ -0,0 +1,12 @@
+//Print Length of String
+import java.util.*;
+ 
+public class PrintLengthofstring {
+   public static void main(String args[]) {
+     StringBuilder sb = new StringBuilder("Tony");
+     sb.append(" Stark");
+     System.out.println(sb); 
+ 
+     System.out.println(sb.length());
+   }
+}
