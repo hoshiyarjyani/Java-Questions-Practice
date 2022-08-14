520591930dc64d36555c47d001bbb1797c5eae0a
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-14 13:45:03 +0530

    Get Bit Method in java

---
+++ setBIT.java
@@ -0,0 +1,11 @@
+public class setBIT {
+    public static void main(String args[]){
+        int n = 5;//0101
+        int position= 1;
+        int bitmask = 1<<position;
+
+        int newNo = n | bitmask; //result = 7 (0111))
+
+        System.out.println(newNo);
+    }
+}
