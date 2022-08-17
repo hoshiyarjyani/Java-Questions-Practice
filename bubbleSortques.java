c54585de11469b0d24936966008deb9cb45dc29c
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-17 15:42:41 +0530

    bubblesort precticee ques

---
+++ bubbleSortques.java
@@ -0,0 +1,23 @@
+public class bubbleSortques {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1,4,6,3,0,9,2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swaping
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArr(arr);
+    }
+}
