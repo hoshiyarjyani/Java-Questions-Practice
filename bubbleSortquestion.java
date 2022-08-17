459d4237faf4a6badf73585b5b0c9db9f6fc83eb
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-17 15:59:21 +0530

    bubblesort precticee question

---
+++ bubbleSortquestion.java
@@ -0,0 +1,23 @@
+public class bubbleSortquestion {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 6, 4, 9, 8, 3 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swaping of elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArr(arr);
+    }
+}
