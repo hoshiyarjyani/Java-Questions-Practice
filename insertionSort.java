d474962d0c7886c82b364cc5942c8b280a2f3d63
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-18 01:14:41 +0530

    Insertionsort prectice question

---
+++ insertionSort.java
@@ -0,0 +1,23 @@
+public class insertionSort {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 9, 6, 3, 5, 2, 8, 1, 6, 0 };
+
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        printArr(arr);
+    }
+}
