c584c7f042cbde45e63c85ed28d31cc012c69f5c
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-18 03:55:58 +0530

    Recursion sum of natural no  print  prectice question

---
+++ sumnaturalnoRecursion.java
@@ -0,0 +1,14 @@
+public class sumnaturalnoRecursion {
+    public static void printSum(int i, int n, int sum){
+        if(i==n){
+            sum+=i;
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        printSum(i+1,n,sum);
+    }
+    public static void main(String args[]){
+       printSum(1, 10, 0);
+    }
+}
