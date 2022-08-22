7ae7f23d2adfacda920402b98a6c27f0c8c9c588
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-22 14:05:11 +0530

    perimeter of a rectangle calculate

---
+++ perimeter.java
@@ -0,0 +1,27 @@
+
+// Calculute perimeter of rectangle in  which  data is input by the user between length >= 1 && breadth <= 100;
+
+
+import java.util.Scanner;
+// we create a function  that calculate the perimeter;
+public class perimeter {
+    public static int calculatePerimeter(int length, int breadth) {
+        int result = 2 * (length + breadth);
+
+        //return because it return a integer type value...we mantion in function;
+        return result;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int breadth = sc.nextInt();
+
+        //if condition for the check the input is correct or not;
+        if (length >= 1 && breadth <= 100) {
+            System.out.println(calculatePerimeter(length, breadth));
+        } else {
+            System.out.println("Please enter value between 'length>=1 && breadth<=100'");
+        }
+    }
+}
