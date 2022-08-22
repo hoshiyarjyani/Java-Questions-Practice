a8db975e679bcabce454a6322b6d3d6222229fe0
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-22 15:27:41 +0530

    Multiply with sum function  input taking  from  user

---
+++ multiplywithsum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class multiplywithsum {
+    //function  for the multiply a b c with k then addition;
+    public static int calculateMultiply(int k, int a, int b, int c){
+
+        // all  a b c first multiply with k. then  multiply  with  each  other;
+        int result = (k*a)+(k*b)+(k*c);
+        return result;
+    }
+    public static void main (String args[]){
+
+        //taking input from  the user;
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+        int b = sc.nextInt();
+
+        // if condition  if our int value 1 to  20  then our function work  otherwise it return invalid;
+        if(1<=k && 1<=a && 1<=b && 1<=c && k<=20 && a<=20 && b<=20 && c<=20){
+            System.out.println(calculateMultiply(k, a, b, c));
+        }else{
+            System.out.println("invalid Value 'your values can be 1 to 20'");
+        }
+    }
+}
+
